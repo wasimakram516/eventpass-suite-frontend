e8322a7bb38d1247cfb751654fdee0239eeaf696
Wasim Akram <mr.saim516@gmail.com>
2025-07-03 14:38:27 +0500

    fixed errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+export default {
     eslint: {
-      ignoreDuringBuilds: true, 
+      ignoreDuringBuilds: true,
     },
   };
-  
-  module.exports = nextConfig;
   
