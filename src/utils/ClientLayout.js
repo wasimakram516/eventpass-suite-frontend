d6e7084409ccc46d1cd87e4605bfb07c4e8c288c
Wasim Akram <mr.saim516@gmail.com>
2025-10-07 12:41:16 +0500

    update

--- src/utils/ClientLayout.js
+++ src/utils/ClientLayout.js
@@ -25,6 +25,7 @@ export default function ClientLayout({ children }) {
     "/checkin",
     "/eventwheel",
     "/surveyguru",
+    "/event"
   ];
 
   const hideNavbar = modulePrefixes.some((prefix) =>
