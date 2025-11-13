96494bebc1756e10bf3673044f095f2fd5eb1e6b
Wasim Akram <mr.saim516@gmail.com>
2025-11-13 22:33:33 +0500

    Update

--- src/app/cms/modules/tapmatch/games/[gameSlug]/page.js
+++ src/app/cms/modules/tapmatch/games/[gameSlug]/page.js
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
 
 export default function Page() {
-  return redirect(`/cms/modules/quiznest/games`);
+  return redirect(`/cms/modules/tapmatch/games`);
 }
