3489b427df55ad489ff656e96c0f24641265fa9f
Talha Faisal <talhafaisal2510@gmail.com>
2025-12-10 21:45:37 +0500

    Update / initialized aws variables

--- src/config/env.js
+++ src/config/env.js
@@ -4,6 +4,13 @@ const env = {
     socket: process.env.NEXT_PUBLIC_WEBSOCKET_HOST,
   },
   node_env: process.env.NEXT_PUBLIC_NODE_ENV || 'development',
+  aws: {
+    region: process.env.NEXT_PUBLIC_AWS_REGION,
+    accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY,
+    s3Bucket: process.env.NEXT_PUBLIC_S3_BUCKET,
+    cloudfrontUrl: process.env.NEXT_PUBLIC_CLOUDFRONT_URL,
+  },
 };
 
 export default env;
