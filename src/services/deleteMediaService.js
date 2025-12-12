d0c24f65d74a26825a6bde2dcfef260ebfdef766
Talha Faisal <talhafaisal2510@gmail.com>
2025-12-12 19:25:32 +0500

    Added / delete option for survey guru fields

--- src/services/deleteMediaService.js
+++ src/services/deleteMediaService.js
@@ -15,6 +15,9 @@ export const deleteMedia = withApiHandler(
         gameId,
         questionId,
         answerImageIndex,
+        formId,
+        questionIndex,
+        optionIndex,
     }) => {
         if (!fileUrl) {
             throw new Error("File URL is required");
@@ -32,6 +35,9 @@ export const deleteMedia = withApiHandler(
         if (gameId) payload.gameId = gameId;
         if (questionId) payload.questionId = questionId;
         if (answerImageIndex !== undefined) payload.answerImageIndex = answerImageIndex;
+        if (formId) payload.formId = formId;
+        if (questionIndex !== undefined) payload.questionIndex = questionIndex;
+        if (optionIndex !== undefined) payload.optionIndex = optionIndex;
 
         const { data } = await api.post("/media/delete", payload);
         return data;
