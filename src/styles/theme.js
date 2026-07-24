e355a4cfec1b24dfba36de1db1911af8712fac5b
Wasim Akram <mr.saim516@gmail.com>
2026-07-24 16:06:20 +0500

    feat: enhance country code selector and picker with localization support and improved search functionality

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1156,7 +1156,12 @@ export const getTheme = (mode = "light", direction = "ltr") => {
                 : "rgba(255,255,255,0.9)",
             },
           },
-          icon: ({ theme }) => ({ color: theme.palette.primary.main, right: 16 }),
+          icon: ({ theme }) => ({
+            color: theme.palette.primary.main,
+            ...(theme.direction === "rtl"
+              ? { left: 16, right: "auto" }
+              : { right: 16, left: "auto" }),
+          }),
         },
       },
     },
