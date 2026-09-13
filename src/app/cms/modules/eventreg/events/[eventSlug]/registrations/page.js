64aa35f058e16ab06241b1d8a7a97e45bb13a73d
Ali Aais <99737152+aais1@users.noreply.github.com>
2026-09-13 18:47:43 +0500

    fix(BadgeCustomization):[EP-238] QR_Visibillity_control and aligment … (#203)
    * fix(BadgeCustomization):[EP-238] QR_Visibillity_control and aligment for QR
    
    * add/badgepdf.js
    
    * fix/resolving PR comments

--- src/app/cms/modules/eventreg/events/[eventSlug]/registrations/page.js
+++ src/app/cms/modules/eventreg/events/[eventSlug]/registrations/page.js
@@ -1343,6 +1343,7 @@ export default function ViewRegistrations() {
         badgeIdentifier: registration.badgeIdentifier || "",
         token: registration.token,
         showQrOnBadge: eventDetails?.showQrOnBadge ?? true,
+        hideTokenOnBadge: eventDetails?.hideTokenOnBadge ?? false,
         customFields: registration.customFields || {},
       };
 
